ebf168dc2d7852b7c1aa96b1d8d50b3c231363f7
Anjana Asokan <anjanaasokan@Anjanas-MacBook-Air.local>
2023-04-17 09:30:13 +0530

    completed coding challenge 1

---
+++ CodingChallenge1/coding1.js
@@ -0,0 +1,13 @@
+/*
+    Write a function that takes the base and height of a triangle and return its area.
+*/
+
+let base = 10
+let height = 5
+
+console.log(triArea(10,5))
+
+function triArea(base,height){
+    let area = (1/2) * base * height
+    return area;
+}
